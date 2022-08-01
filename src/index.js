0f8cd6f6e7400316557c88994dce42ec3df52eb2
AlexGMarinho <alexg.marinhoooo>
2022-07-31 23:40:28 -0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import serviceSearchFactPerYear from './functions/services.js';
+import validServiceYear from './functions/validator.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const { year } = req.query;
+
+    if (validServiceYear(year)) {
+        const fact = serviceSearchFactPerYear(year);
+
+        return res.json({ fact: fact });
+    }
+
+    return res.status(400).json({ Message: 'Invalid year' });
+});
+
+app.listen(8080, () => {
+    console.log(`Servidor escutando na porta 8080`);
+});
