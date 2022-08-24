ed89db211d642db34fb35deb5d36847906336355
AlexG.Marinho <106933144+AlexGMarinho@users.noreply.github.com>
2022-08-24 14:50:19 -0300

    Update Service.js

--- src/functions/services.js
+++ src/functions/services.js
@@ -1,7 +1,7 @@
 import historicalFacts from '../dataBase/facts.js';
 
 function serviceSearchFactPerYear(year) {
-    let factSelected = historicalFacts.find((fact) => {
+    const factSelected = historicalFacts.find((fact) => {
         return fact.year === year;
     });
     return factSelected.fact;
